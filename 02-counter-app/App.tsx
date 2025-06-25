4f07bb4bad5cb19426203a784ae8b30cb11294de
chetex <chetex69@gmail.com>
2025-06-25 17:28:15 +0200

    increment, counter and button

--- 02-counter-app/App.tsx
+++ 02-counter-app/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  // Create counter using useState
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      // Create a button to increment the counter
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text style={{ color: 'white', fontSize: 24, alignContent: "center" }}>+1</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  textHuge: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+
+  // Move the button to the bottom of the screen
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: 60,
+    height: 60, 
   },
 });
